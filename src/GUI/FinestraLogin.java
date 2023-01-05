f605e0e460272946a986e24268537a991da9a2a0
armandoegger1 <53280440+armandoegger1@users.noreply.github.com>
2023-01-05 17:43:55 +0100

    Ridefinito il Layout di FinestraLogin
    Co-Authored-By: franciflores <121508356+franciflores@users.noreply.github.com>

--- src/GUI/FinestraLogin.java
+++ src/GUI/FinestraLogin.java
@@ -1,75 +1,120 @@
 package GUI;
-import Classi.Controller;
+
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import java.awt.BorderLayout;
-import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
 
-
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
+import Classi.Controller;
+import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.SwingConstants;
+import javax.swing.DropMode;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
-public class FinestraLogin extends JFrame{
+public class FinestraLogin extends JFrame {
 
-	private JFrame FrameLogin;
-	private JTextField textFieldMatricola;
-	private JTextField textFieldPassword;
-	
+	private JPanel contentPane;
 	private Controller controller;
+	private JTextField textFieldMatricola;
+	private JPasswordField passwordFieldMatricola;
 
-	//Finestra Login
+	/**
+	 * Create the frame.
+	 * @param controller 
+	 */
 	public FinestraLogin(Controller c) {
+		setResizable(false);
 		controller = c;
-		initialize();
-	}
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
-	//Inizializza la Finestra
-	private void initialize() {
-		FrameLogin = new JFrame();
-		FrameLogin.setResizable(false);
-		FrameLogin.setTitle("Login");
-		FrameLogin.setBounds(100, 100, 640, 480);
-		FrameLogin.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{212, 0, 212, 0};
+		gbl_contentPane.rowHeights = new int[]{62, 62, 62, 62, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
 		
-		JPanel PanelLogin = new JPanel();
-		FrameLogin.getContentPane().add(PanelLogin, BorderLayout.CENTER);
-		PanelLogin.setLayout(null);
+		JLabel lblBenvenuto = new JLabel("Esegui il Login");
+		lblBenvenuto.setFont(new Font("Verdana", Font.BOLD, 17));
+		lblBenvenuto.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblBenvenuto = new GridBagConstraints();
+		gbc_lblBenvenuto.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBenvenuto.gridx = 1;
+		gbc_lblBenvenuto.gridy = 0;
+		contentPane.add(lblBenvenuto, gbc_lblBenvenuto);
+		
+		JLabel lblMatricola = new JLabel("Matricola");
+		lblMatricola.setToolTipText("");
+		lblMatricola.setFont(new Font("Verdana", Font.PLAIN, 14));
+		lblMatricola.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblMatricola = new GridBagConstraints();
+		gbc_lblMatricola.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMatricola.gridx = 0;
+		gbc_lblMatricola.gridy = 1;
+		contentPane.add(lblMatricola, gbc_lblMatricola);
 		
 		textFieldMatricola = new JTextField();
-		textFieldMatricola.setBounds(141, 159, 350, 20);
-		PanelLogin.add(textFieldMatricola);
+		textFieldMatricola.setToolTipText("Inserisci Matricola");
+		lblMatricola.setLabelFor(textFieldMatricola);
+		textFieldMatricola.setFont(new Font("Verdana", Font.PLAIN, 14));
+		textFieldMatricola.setHorizontalAlignment(SwingConstants.CENTER);
 		textFieldMatricola.setColumns(10);
+		GridBagConstraints gbc_textFieldMatricola = new GridBagConstraints();
+		gbc_textFieldMatricola.gridwidth = 2;
+		gbc_textFieldMatricola.ipadx = 99;
+		gbc_textFieldMatricola.weighty = 1.0;
+		gbc_textFieldMatricola.weightx = 2.0;
+		gbc_textFieldMatricola.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldMatricola.gridx = 1;
+		gbc_textFieldMatricola.gridy = 1;
+		contentPane.add(textFieldMatricola, gbc_textFieldMatricola);
 		
-		JLabel loginTextLabel = new JLabel("Effettua il Login");
-		loginTextLabel.setLabelFor(textFieldMatricola);
-		loginTextLabel.setBounds(269, 134, 80, 14);
-		PanelLogin.add(loginTextLabel);
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Verdana", Font.PLAIN, 14));
+		lblPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_lblPassword = new GridBagConstraints();
+		gbc_lblPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_lblPassword.gridx = 0;
+		gbc_lblPassword.gridy = 2;
+		contentPane.add(lblPassword, gbc_lblPassword);
 		
-		JLabel matricolaLabel = new JLabel("Matricola");
-		matricolaLabel.setBounds(85, 162, 46, 14);
-		PanelLogin.add(matricolaLabel);
+		passwordFieldMatricola = new JPasswordField();
+		lblPassword.setLabelFor(passwordFieldMatricola);
+		passwordFieldMatricola.setFont(new Font("Verdana", Font.PLAIN, 14));
+		passwordFieldMatricola.setHorizontalAlignment(SwingConstants.CENTER);
+		GridBagConstraints gbc_passwordFieldMatricola = new GridBagConstraints();
+		gbc_passwordFieldMatricola.gridwidth = 2;
+		gbc_passwordFieldMatricola.ipadx = 99;
+		gbc_passwordFieldMatricola.insets = new Insets(0, 0, 5, 0);
+		gbc_passwordFieldMatricola.gridx = 1;
+		gbc_passwordFieldMatricola.gridy = 2;
+		contentPane.add(passwordFieldMatricola, gbc_passwordFieldMatricola);
 		
-		JLabel passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(85, 208, 46, 14);
-		PanelLogin.add(passwordLabel);
-		
-		textFieldPassword = new JTextField();
-		textFieldPassword.setBounds(141, 205, 350, 20);
-		PanelLogin.add(textFieldPassword);
-		textFieldPassword.setColumns(10);
-		
-		JButton btnLogin = new JButton("Login");
-		btnLogin.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnLogin.setBounds(260, 236, 89, 23);
-		PanelLogin.add(btnLogin);
+		JButton btnConferma = new JButton("Accedi");
+		btnConferma.setFont(new Font("Verdana", Font.PLAIN, 14));
+		GridBagConstraints gbc_btnConferma = new GridBagConstraints();
+		gbc_btnConferma.gridx = 2;
+		gbc_btnConferma.gridy = 3;
+		contentPane.add(btnConferma, gbc_btnConferma);
 	}
+
 }
