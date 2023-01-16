3112f999f8bd54aa1347e7a98e7da9177b491dd1
franciflores <francifiore2000@hotmail.it>
2023-01-16 11:47:56 +0100

    Commit per restorare l'ultima versione del programma

--- src/GUI/MenuOperazioni.java
+++ src/GUI/MenuOperazioni.java
@@ -23,6 +23,8 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
+import java.awt.Component;
+import javax.swing.Box;
 
 public class MenuOperazioni extends JFrame {
 
@@ -45,45 +47,26 @@ public class MenuOperazioni extends JFrame {
 		JLabel lblAzioni = new JLabel("\"qui metteremo 'benvenuto x' con il nome dell'utente\"");
 		lblAzioni.setHorizontalAlignment(SwingConstants.CENTER);
 		lblAzioni.setBounds(10, 5, 464, 18);
-		lblAzioni.setFont(new Font("Gill Sans Nova", Font.BOLD, 14));
+		lblAzioni.setFont(new Font("Tahoma", Font.BOLD, 13));
 		contentPane.add(lblAzioni);
 		
 		JButton btnNuovaTartaruga = new JButton("Nuova Tartaruga");
-		btnNuovaTartaruga.setBounds(151, 59, 171, 25);
+		btnNuovaTartaruga.setBounds(178, 61, 124, 21);
 		btnNuovaTartaruga.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+				//Cambiare con un setter questa Variabile
+				controller.SceltaPanel = controller.DialogTartarughe();
 				controller.ShowTartarugaFrame();
 			}
 		});
-		btnNuovaTartaruga.setFont(new Font("Verdana", Font.PLAIN, 13));
+		btnNuovaTartaruga.setFont(new Font("Tahoma", Font.PLAIN, 11));
 		contentPane.add(btnNuovaTartaruga);
 		
-		JButton btnCreaVasca = new JButton("Nuova Vasca");
-		btnCreaVasca.setEnabled(false);
-		btnCreaVasca.setBounds(10, 59, 135, 25);
-		btnCreaVasca.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controller.ShowVascaFrame();
-			}
-		});
-		btnCreaVasca.setFont(new Font("Verdana", Font.PLAIN, 13));
-		contentPane.add(btnCreaVasca);
-		
 		JLabel lblOperazioni = new JLabel("Operazioni");
-		lblOperazioni.setFont(new Font("Verdana", Font.BOLD, 14));
-		lblOperazioni.setBounds(200, 34, 83, 18);
+		lblOperazioni.setHorizontalAlignment(SwingConstants.CENTER);
+		lblOperazioni.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblOperazioni.setBounds(10, 34, 464, 18);
 		contentPane.add(lblOperazioni);
-		
-		JButton btnCompilaCartella = new JButton("Compila Cartella");
-		btnCompilaCartella.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				controller.CreaCartella();
-			}
-		});
-		btnCompilaCartella.setFont(new Font("Verdana", Font.PLAIN, 13));
-		btnCompilaCartella.setBounds(328, 59, 146, 25);
-		contentPane.add(btnCompilaCartella);
 		
 		JButton btnLogOut = new JButton("Log Out");
 		btnLogOut.addActionListener(new ActionListener() {
@@ -91,14 +74,78 @@ public class MenuOperazioni extends JFrame {
 				controller.RitornoAlLogin();
 			}
 		});
-		btnLogOut.setFont(new Font("Verdana", Font.PLAIN, 13));
-		btnLogOut.setBounds(370, 320, 89, 23);
+		btnLogOut.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnLogOut.setBounds(397, 332, 77, 18);
 		contentPane.add(btnLogOut);
-	}
-
-
-	public void ApriOptionPaneTartaruga() {
-		JOptionPane DialogTart = new JOptionPane("E' la prima volta che prendiamo in cura questa tartaruga?",JOptionPane.QUESTION_MESSAGE,JOptionPane.YES_NO_OPTION);
-		//Ora devo fare in modo di prendere il valore YES,NO come valore e fare un IF
+		
+		JButton btnStatoVasche = new JButton("Info sul Centro");
+		btnStatoVasche.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnStatoVasche.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnStatoVasche.setBounds(10, 61, 117, 21);
+		contentPane.add(btnStatoVasche);
+		
+		JButton btnCompilaCartelle = new JButton("Vedi Statistiche");
+		btnCompilaCartelle.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnCompilaCartelle.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnCompilaCartelle.setBounds(350, 61, 124, 21);
+		contentPane.add(btnCompilaCartelle);
+		
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		horizontalStrut.setBounds(125, 63, 54, 19);
+		contentPane.add(horizontalStrut);
+		
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		horizontalStrut_1.setBounds(298, 61, 54, 19);
+		contentPane.add(horizontalStrut_1);
+		
+		JLabel lblNewLabel = new JLabel("Operatore");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(20, 93, 85, 14);
+		contentPane.add(lblNewLabel);
+		
+		Component verticalStrut = Box.createVerticalStrut(20);
+		verticalStrut.setBounds(115, 87, 12, 263);
+		contentPane.add(verticalStrut);
+		
+		JLabel lblNewLabel_1 = new JLabel("Medico");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setBounds(135, 93, 91, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		horizontalStrut_2.setBounds(10, 296, 464, 18);
+		contentPane.add(horizontalStrut_2);
+		
+		JLabel lblNewLabel_2 = new JLabel("Tecnico Laboratorio");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(258, 93, 94, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Ricercatore");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_3.setBounds(382, 93, 92, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("che cosa fa l'operatore?");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(16, 113, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		Component verticalStrut_1 = Box.createVerticalStrut(20);
+		verticalStrut_1.setBounds(236, 87, 12, 263);
+		contentPane.add(verticalStrut_1);
+		
+		Component verticalStrut_2 = Box.createVerticalStrut(20);
+		verticalStrut_2.setBounds(360, 93, 12, 263);
+		contentPane.add(verticalStrut_2);
 	}
 }
